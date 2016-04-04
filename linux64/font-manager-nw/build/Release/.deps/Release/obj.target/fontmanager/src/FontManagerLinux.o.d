0debeb74adfe4b748eb9d461e6dcc03af4bb864f
Ryan <ryan@burgettweb.net>
2016-04-04 16:06:36 -0400

    Added files

---
+++ linux64/font-manager-nw/build/Release/.deps/Release/obj.target/fontmanager/src/FontManagerLinux.o.d
@@ -0,0 +1,59 @@
+cmd_Release/obj.target/fontmanager/src/FontManagerLinux.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/ryan/.nw-gyp/0.12.3/src -I/home/ryan/.nw-gyp/0.12.3/deps/uv/include -I/home/ryan/.nw-gyp/0.12.3/deps/v8/include -I../node_modules/nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -ffunction-sections -fdata-sections -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/fontmanager/src/FontManagerLinux.o.d.raw  -c -o Release/obj.target/fontmanager/src/FontManagerLinux.o ../src/FontManagerLinux.cc
+Release/obj.target/fontmanager/src/FontManagerLinux.o: \
+ ../src/FontManagerLinux.cc ../src/FontDescriptor.h \
+ /home/ryan/.nw-gyp/0.12.3/src/node.h \
+ /home/ryan/.nw-gyp/0.12.3/src/../deps/uv/include/uv.h \
+ /home/ryan/.nw-gyp/0.12.3/src/../deps/uv/include/uv-errno.h \
+ /home/ryan/.nw-gyp/0.12.3/src/../deps/uv/include/uv-version.h \
+ /home/ryan/.nw-gyp/0.12.3/src/../deps/uv/include/uv-unix.h \
+ /home/ryan/.nw-gyp/0.12.3/src/../deps/uv/include/uv-threadpool.h \
+ /home/ryan/.nw-gyp/0.12.3/src/../deps/uv/include/uv-linux.h \
+ /home/ryan/.nw-gyp/0.12.3/deps/v8/include/v8.h \
+ /home/ryan/.nw-gyp/0.12.3/deps/v8/include/v8config.h \
+ /home/ryan/.nw-gyp/0.12.3/src/node_version.h ../node_modules/nan/nan.h \
+ /home/ryan/.nw-gyp/0.12.3/src/node_version.h \
+ /home/ryan/.nw-gyp/0.12.3/deps/uv/include/uv.h \
+ /home/ryan/.nw-gyp/0.12.3/src/node_buffer.h \
+ /home/ryan/.nw-gyp/0.12.3/src/node.h \
+ /home/ryan/.nw-gyp/0.12.3/src/smalloc.h \
+ /home/ryan/.nw-gyp/0.12.3/src/node_object_wrap.h \
+ ../node_modules/nan/nan_callbacks.h \
+ ../node_modules/nan/nan_callbacks_12_inl.h \
+ ../node_modules/nan/nan_maybe_pre_43_inl.h \
+ ../node_modules/nan/nan_converters.h \
+ ../node_modules/nan/nan_converters_pre_43_inl.h \
+ ../node_modules/nan/nan_new.h \
+ ../node_modules/nan/nan_implementation_12_inl.h \
+ ../node_modules/nan/nan_persistent_12_inl.h \
+ ../node_modules/nan/nan_weak.h ../node_modules/nan/nan_object_wrap.h \
+ ../node_modules/nan/nan_typedarray_contents.h
+../src/FontManagerLinux.cc:
+../src/FontDescriptor.h:
+/home/ryan/.nw-gyp/0.12.3/src/node.h:
+/home/ryan/.nw-gyp/0.12.3/src/../deps/uv/include/uv.h:
+/home/ryan/.nw-gyp/0.12.3/src/../deps/uv/include/uv-errno.h:
+/home/ryan/.nw-gyp/0.12.3/src/../deps/uv/include/uv-version.h:
+/home/ryan/.nw-gyp/0.12.3/src/../deps/uv/include/uv-unix.h:
+/home/ryan/.nw-gyp/0.12.3/src/../deps/uv/include/uv-threadpool.h:
+/home/ryan/.nw-gyp/0.12.3/src/../deps/uv/include/uv-linux.h:
+/home/ryan/.nw-gyp/0.12.3/deps/v8/include/v8.h:
+/home/ryan/.nw-gyp/0.12.3/deps/v8/include/v8config.h:
+/home/ryan/.nw-gyp/0.12.3/src/node_version.h:
+../node_modules/nan/nan.h:
+/home/ryan/.nw-gyp/0.12.3/src/node_version.h:
+/home/ryan/.nw-gyp/0.12.3/deps/uv/include/uv.h:
+/home/ryan/.nw-gyp/0.12.3/src/node_buffer.h:
+/home/ryan/.nw-gyp/0.12.3/src/node.h:
+/home/ryan/.nw-gyp/0.12.3/src/smalloc.h:
+/home/ryan/.nw-gyp/0.12.3/src/node_object_wrap.h:
+../node_modules/nan/nan_callbacks.h:
+../node_modules/nan/nan_callbacks_12_inl.h:
+../node_modules/nan/nan_maybe_pre_43_inl.h:
+../node_modules/nan/nan_converters.h:
+../node_modules/nan/nan_converters_pre_43_inl.h:
+../node_modules/nan/nan_new.h:
+../node_modules/nan/nan_implementation_12_inl.h:
+../node_modules/nan/nan_persistent_12_inl.h:
+../node_modules/nan/nan_weak.h:
+../node_modules/nan/nan_object_wrap.h:
+../node_modules/nan/nan_typedarray_contents.h:
